6513cd081ca0072a977537b8d934e4b7da9782f1
Immanuel Albrecht <immanuel@perazim.ch>
2022-07-09 19:30:09 +0200

    The worldgen is now working as intended. Next target is to implement global parameters for the relative frequency and let them be globally fulfilled, while still allowing for local divergences.

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -3,13 +3,13 @@ use rand::Rng;
 
 use crate::{ascii::{AsciiSheet, spawn_ascii_sprite}, player::Player, TILE_SIZE};
 
-const LATTICE_RADIUS: i32 = 4;
-const SPAWN_RADIUS: i32 = 2;
+const SPAWN_RADIUS: i32 = 10;
+const CHECK_RADIUS: i32 = 2;
 const TILE_Z: f32 = 100.0;
 
-const DIRT_MODIFIER: usize = 1;
-const GRASS_MODIFIER: usize = 3;
-const STONE_MODIFIER: usize = 1;
+const DIRT_MODIFIER: usize = 40;
+const GRASS_MODIFIER: usize = 10;
+const STONE_MODIFIER: usize = 40;
 
 #[derive(Component)]
 pub struct Map;
@@ -88,11 +88,11 @@ fn spawn_tile(
     new_tile: MapTile,
     tile_query: &Query<(&MapTile, &TileType), Without<Map>>,
 ) -> Entity {
-    // Find neighbours
     let (mut dirtcount, mut grasscount, mut stonecount): (usize, usize, usize) = (1, 1, 1);
 
+    // Find neighbours
     for (tile, tile_type) in tile_query.iter() {
-        if (tile.0.x - new_tile.0.x) * (tile.0.x - new_tile.0.x) + (tile.0.y - new_tile.0.y) * (tile.0.y - new_tile.0.y) < SPAWN_RADIUS * SPAWN_RADIUS {
+        if ((tile.0.x - new_tile.0.x) * (tile.0.x - new_tile.0.x)) + ((tile.0.y - new_tile.0.y) * (tile.0.y - new_tile.0.y)) < CHECK_RADIUS * CHECK_RADIUS {
             match tile_type {
                 TileType::Dirt => { dirtcount += DIRT_MODIFIER },
                 TileType::Grass => { grasscount += GRASS_MODIFIER },
@@ -101,26 +101,23 @@ fn spawn_tile(
         }
     }
 
-    let mut rng = rand::thread_rng();
-    let draw = rng.gen_range(0..(dirtcount + grasscount + stonecount));
-    let case = match draw {
+    let draw: usize = rand::thread_rng().gen_range(0..(dirtcount + grasscount + stonecount));
+    let tile_type: TileType = match draw {
         n if n < dirtcount => { TileType::Dirt },
-        n if dirtcount <= n && n < grasscount => {TileType::Grass}
+        n if n < dirtcount + grasscount => {TileType::Grass}
         _ => {TileType::Stone},
     };
 
-    let tile: Entity = match case {
-        TileType::Dirt => spawn_dirt(commands, ascii, new_tile),
-        TileType::Grass => spawn_grass(commands, ascii, new_tile),
-        TileType::Stone => spawn_tile_type(commands, ascii, new_tile,
-            176,
-            Color::rgb(192.0 / 255.0, 192.0 / 255.0, 192.0 / 255.0),
-            "Stone".to_owned()),
-    };
-    tile
+    spawn_tile_type(commands, ascii, new_tile, tile_type)
 }
 
-fn spawn_tile_type(commands: &mut Commands, ascii: &Res<AsciiSheet>, new_tile: MapTile, index: usize, color: Color, name: String) -> Entity {
+fn spawn_tile_type(commands: &mut Commands, ascii: &Res<AsciiSheet>, new_tile: MapTile, tile_type: TileType) -> Entity {
+    let (index, color, name) = match tile_type {
+        TileType::Dirt => ('#' as usize, Color::rgb(194.0 / 255.0, 126.0 / 255.0, 64.0 / 255.0), "Dirt".to_owned()),
+        TileType::Grass => ('~' as usize, Color::LIME_GREEN, "Grass".to_owned()),
+        TileType::Stone => (176, Color::rgb(192.0 / 255.0, 192.0 / 255.0, 192.0 / 255.0), "Stone".to_owned()),
+    };
+    
     let sprite = spawn_ascii_sprite(
         commands,
         ascii,
@@ -130,40 +127,8 @@ fn spawn_tile_type(commands: &mut Commands, ascii: &Res<AsciiSheet>, new_tile: M
     );
     let tile = commands.entity(sprite)
         .insert(MapTile(new_tile.0))
-        .insert(TileType::Dirt)
+        .insert(tile_type)
         .insert(Name::new(name +  &": " + new_tile.0.x.to_string().as_str() + " " + new_tile.0.y.to_string().as_str()))
-        .id();
-    tile
-}
-
-fn spawn_grass(commands: &mut Commands, ascii: &Res<AsciiSheet>, new_tile: MapTile) -> Entity {
-    let sprite = spawn_ascii_sprite(
-        commands,
-        ascii,
-        '~' as usize,
-        Color::LIME_GREEN,
-        new_tile.0.as_vec2().extend(TILE_Z) * TILE_SIZE,
-    );
-    let tile = commands.entity(sprite)
-        .insert(MapTile(new_tile.0))
-        .insert(TileType::Dirt)
-        .insert(Name::new("Grass: ".to_owned() + new_tile.0.x.to_string().as_str() + " " + new_tile.0.y.to_string().as_str()))
-        .id();
-    tile
-}
-
-fn spawn_dirt(commands: &mut Commands, ascii: &Res<AsciiSheet>, new_tile: MapTile) -> Entity {
-    let sprite = spawn_ascii_sprite(
-        commands,
-        ascii,
-        '#' as usize,
-        Color::rgb(194.0 / 255.0, 126.0 / 255.0, 64.0 / 255.0),
-        new_tile.0.as_vec2().extend(TILE_Z) * TILE_SIZE,
-    );
-    let tile = commands.entity(sprite)
-        .insert(MapTile(new_tile.0))
-        .insert(TileType::Dirt)
-        .insert(Name::new("Dirt: ".to_owned() + new_tile.0.x.to_string().as_str() + " " + new_tile.0.y.to_string().as_str()))
         .id();
     tile
 }
@@ -178,11 +143,9 @@ fn spawn_map(mut commands: Commands, ascii: Res<AsciiSheet>) {
     
     let mut tiles: Vec<Entity> = Vec::new();
 
-    tiles.push(spawn_dirt(
-        &mut commands,
-        &ascii,
-        MapTile(IVec2::new(0, 0))
-    ));
+    tiles.push(
+        spawn_tile_type(&mut commands, &ascii, MapTile(IVec2::new(0, 0)), TileType::Grass)
+    );
     
     commands.entity(map).push_children(&tiles);
 }
@@ -194,14 +157,14 @@ fn get_lattice_points_in_radius(pos: Vec3) -> Vec<MapTile> {
 
     let mut points: Vec<MapTile> = Vec::new();
 
-    for x in -LATTICE_RADIUS..LATTICE_RADIUS+1 {
-        for y in -LATTICE_RADIUS..LATTICE_RADIUS+1 {
+    for x in -SPAWN_RADIUS..SPAWN_RADIUS+1 {
+        for y in -SPAWN_RADIUS..SPAWN_RADIUS+1 {
             points.push(MapTile(IVec2::new(x, y)));
         }
     }
 
     let points = points.iter()
-        .filter(|tile| tile.0.x * tile.0.x + tile.0.y * tile.0.y < LATTICE_RADIUS * LATTICE_RADIUS )
+        .filter(|tile| tile.0.x * tile.0.x + tile.0.y * tile.0.y < SPAWN_RADIUS * SPAWN_RADIUS )
         .map(|tile| MapTile(tile.0 + pos))
         .collect();
     
